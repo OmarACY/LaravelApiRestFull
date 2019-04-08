e2948447bc55c8470957dc144aa05f9574e69521
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2019-04-07 23:43:48 -0500

    Correcciones para de migraciones y implementacion de los Seeders

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('verified')->default(User::USUARIO_NO_VERIFICADO);
-            $table->string('verified')->nullable();
+            $table->string('verification_token')->nullable();
             $table->string('admin')->default(User::USUARIO_REGULAR);
             $table->timestamps();
         });
