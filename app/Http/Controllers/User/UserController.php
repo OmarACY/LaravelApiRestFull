51a533132f9f3c9eef53f646667330d1ad9d9efc
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2019-04-08 01:29:05 -0500

    Actualizando usuarios

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -69,7 +69,46 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user = User::findOrFail($user->id);
+
+        $rules = [
+            'email' => 'email|unique:users,email,' . $user->id,
+            'password' => 'min:6|confirmed',
+            'admin' => 'in:' . User::USUARIO_ADMMINISTRADOR . ',' . User::USUARIO_REGULAR,
+        ];
+
+        $this->validate($request, $rules);
+
+        if($request->has('name')){
+            $user->name = $request->name;
+        }
+
+        if($request->has('password')){
+            $user->password = bcrypt($request->password);
+        }
+
+        if($request->has('admin')){
+            if(!$user->esVerificado()){
+                return response()->json(['error' => 'Unicamente los usuarios verificados pueden cambiar su valor de administrador', 'code' => 409], 409);
+            }
+
+            $user->admin = $request->admin;
+        }
+
+        if($request->has('email') && $user->email != $request->email){
+            $user->verified = User::USUARIO_NO_VERIFICADO;
+            $user->verification_token | User::generarVerificationToken();
+            $user->email = $request->email;
+        }
+
+        if(!$user->isDirty()){
+            return response()->json(['error' => 'Se debe especificar al menos un valor diferente para actualizar', 'code' => 422], 422);
+        }
+
+        $user->save();
+
+        return response()->json(['data' => $user], 200);
+
     }
 
     /**
