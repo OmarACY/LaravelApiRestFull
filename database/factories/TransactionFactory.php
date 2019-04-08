80bc2e911778b61dd2a816157c3bba5e6d1f8b75
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2019-04-07 23:22:07 -0500

    Creando Factory para Transaction

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -1,9 +1,19 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Transaction;
+use App\Seller;
+use App\User;
 
-$factory->define(App\Transaction::class, function (Faker $faker) {
+$factory->define(Transaction::class, function (Faker $faker) {
+
+    $vendedor = Seller::has('products')->get()->random();
+    $comprador = User::all()->except($vendedor->id)->random();
+
     return [
-        //
+        'name' => $faker->name,
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $comprador->id,
+        'product_id' => $vendedor->id,
     ];
 });
