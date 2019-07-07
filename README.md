d708bf0aab5950208e48ff31c99604fee436ab3b
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2019-07-06 19:15:07 -0500

    82. Implementando el Método Index de ProductController

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Laravel API RESTFULL
+
+Crear nuevo controlador ligado a un modelo
+
+```bash
+php artisan make:controller Product/ProductoController -r -m Product
+```
--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -15,18 +15,9 @@ class ProductController extends ApiController
      */
     public function index()
     {
-        //
-    }
+        $products = Product::all();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return $this->showAll($products);
     }
 
     /**
@@ -36,29 +27,6 @@ class ProductController extends ApiController
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
     {
         //
     }
