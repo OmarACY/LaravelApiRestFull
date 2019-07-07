ed13ce34943b040f8f3d3ec8c2de485ae9e09712
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2019-07-07 15:13:56 -0500

    91. Implementando el Método Index de BuyerCategoryController

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ use Illuminate\Http\Request;
  * Buyers
  */
 Route::apiResource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::apiResource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
 Route::apiResource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
 Route::apiResource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
 Route::apiResource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
