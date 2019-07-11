394c3c00543098ec60ca6e5d4c9082404dd3feed
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2019-07-11 17:12:43 -0500

    206. Permitiendo CORS Únicamente en la API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Asm89\Stack\CorsService;
 use Exception;
 use App\Traits\ApiResponser;
 use Illuminate\Database\QueryException;
@@ -60,9 +61,10 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         $response = $this->handleException($request, $exception);
-        //app(CorsService::class)->addActualRequestHeaders($response, $request);
+        app(CorsService::class)->addActualRequestHeaders($response, $request);
         return $response;
     }
+
     public function handleException($request, Exception $exception)
     {
         if ($exception instanceof ValidationException) {
